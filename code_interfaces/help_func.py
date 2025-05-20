52bbed3ecde70ac9b1bb060fa6768143c37b9841
ES <55274511+ESBehtev@users.noreply.github.com>
2025-05-20 03:41:28 +0300

    Вспомогательная функция генерации узлов

--- code_interfaces/help_func.py
+++ code_interfaces/help_func.py
@@ -17,8 +17,22 @@ def generate_noisy_data(func, noise_param=0.0, x_range=(0, 10), n_points=50, ret
 	Returns:
 		x, y —  данные
 		x_dense, y_true — (если return_true_curve=True) плотная сетка и истинная функция
-	"""
 
+	Пример использования:
+		f = np.sin
+		x_data, y_data, x_dense, y_true = generate_noisy_data(f, noise_param=0.1, x_range=(0, 10), n_points=10)
+
+		# Построение графика
+		plt.figure(figsize=(10, 6))
+		plt.plot(x_dense, y_true, '--',color = 'green' ,label=f'Истинный {f.__name__}(x)')
+		plt.scatter(x_data, y_data, color='purple', label='Исходные точки')
+
+		plt.xlabel('x')
+		plt.ylabel(f'{f.__name__}(x)')
+		plt.legend()
+		plt.grid(True)
+		plt.show()
+	"""
 	# функция шума
 	def w_noise(a, N):
 		return a * np.random.normal(0, 1, N)
